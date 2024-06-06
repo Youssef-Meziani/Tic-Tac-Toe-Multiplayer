d82e5f42095c0dce9ba4268261603054b65d2d06
Youssef-Meziani <mezianiyoussef564@gmail.com>
2024-06-06 22:34:57 +0100

    Game and Server projects created

---
+++ Server/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ma.game.tictactoeserver {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ma.game.tictactoeserver to javafx.fxml;
+    exports ma.game.tictactoeserver;
+}
