b4b86cad82e09aeda9c05381d0b1b0cd5ceb86b7
Youssef-Meziani <mezianiyoussef564@gmail.com>
2024-06-09 15:41:55 +0100

    icon added to server

--- Server/src/main/java/ma/game/tictactoeserver/Application.java
+++ Server/src/main/java/ma/game/tictactoeserver/Application.java
@@ -2,6 +2,7 @@ package ma.game.tictactoeserver;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -9,8 +10,10 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Image icon = new Image(getClass().getResourceAsStream("/icon.png"));
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("control-panel.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 300, 200);
+        stage.getIcons().add(icon);
         stage.setTitle("Tic Tac Toe Server");
         stage.setResizable(false);
         stage.setScene(scene);
